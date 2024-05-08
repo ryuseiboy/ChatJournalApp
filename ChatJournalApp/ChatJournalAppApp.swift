0f2e8642f69b86ec1aa938b0a91aa8864c23a264
PianoMan <c0b211626f@edu.teu.ac.jp>
2024-05-08 15:47:43 +0900

    Initial Commit

---
+++ ChatJournalApp/ChatJournalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatJournalAppApp.swift
+//  ChatJournalApp
+//
+//  Created by Takumi Yokawa on 2024/05/08.
+//
+
+import SwiftUI
+
+@main
+struct ChatJournalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
