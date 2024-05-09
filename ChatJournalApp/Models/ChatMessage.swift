03d26887f8f6f5c5d2fc4c7ff7a51893aa08469c
PianoMan <c0b211626f@edu.teu.ac.jp>
2024-05-10 04:22:21 +0900

    ChatMessage構造体のプロパティを定義

--- ChatJournalApp/Models/ChatMessage.swift
+++ ChatJournalApp/Models/ChatMessage.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+public enum ChatRole {
+    case system
+    case user
+}
+
+public struct ChatMessage: Identifiable {
+    public var id = UUID().uuidString
+    public let role: ChatRole
+    public let content: String?
+    
+    public init(role: ChatRole, content: String? = nil) {
+        self.role = role
+        self.content = content
+    }
+}
