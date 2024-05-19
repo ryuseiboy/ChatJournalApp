f279d54b78e9cf08f5304b3338e0dcaece20dacc
PianoMan <c0b211626f@edu.teu.ac.jp>
2024-05-19 22:20:48 +0900

    ホーム画面のボタンとリストの位置関係を修正

---
+++ ChatJournalApp/Views/JournalPreview.swift
@@ -0,0 +1,18 @@
+//
+//  JournalPreview.swift
+//  ChatJournalApp
+//
+//  Created by Takumi Yokawa on 2024/05/19.
+//
+
+import SwiftUI
+
+struct JournalPreview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JournalPreview()
+}
