ad2fe46fdd6ca232010ef73ca011d6e9f3b2d553
PianoMan <c0b211626f@edu.teu.ac.jp>
2024-05-24 00:00:22 +0900

    日記プレビューの編集

--- ChatJournalApp/Views/JournalPreview.swift
+++ ChatJournalApp/Views/JournalPreview.swift
@@ -16,54 +16,98 @@ struct CustomGroupBoxStyle: GroupBoxStyle {
 }
 
 struct JournalPreview: View {
+    @State private var images: [String?] = ["circle.square","circle.square","circle.square","circle.square",nil]
     var body: some View {
         VStack {
             GroupBox(label: Label("User Information", systemImage: "person.circle")) {
-                VStack(alignment: .leading) {
-                    Text("Name: John Doe")
-                    Text("Age: 29")
-                    Text("Location: New York")
+                VStack{
+                    HStack{
+                        if let name = images[0] {
+                            Image(systemName: name)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(maxWidth: .infinity)
+                                .gridCellColumns(2)
+                        }
+                        VStack{
+                            HStack{
+                                if let name = images[1] {
+                                    Image(systemName: name)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(maxWidth: .infinity)
+                                        .gridCellColumns(2)
+                                }
+                                if let name = images[4] {
+                                    Image(systemName: name)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(maxWidth: .infinity)
+                                        .gridCellColumns(2)
+                                }
+                            }
+                            HStack{
+                                if let name = images[2] {
+                                    Image(systemName: name)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(maxWidth: .infinity)
+                                        .gridCellColumns(2)
+                                }
+                                if let name = images[3] {
+                                    Image(systemName: name)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(maxWidth: .infinity)
+                                        .gridCellColumns(2)
+                                }
+                                
+                            }
+                        }
+                        .padding()
+                        
+                    }
+                    
+                }
+                .groupBoxStyle(DefaultGroupBoxStyle())
+                .padding()
+                
+                GroupBox {
+                    Text("This is another group without a label.")
+                        .padding()
                 }
                 .padding()
-            }
-            .groupBoxStyle(DefaultGroupBoxStyle())
-            .padding()
-
-            GroupBox {
-                Text("This is another group without a label.")
+                
+                GroupBox(label: Label("User Information", systemImage: "person.circle")) {
+                    VStack(alignment: .leading) {
+                        Text("Name: John Doe")
+                        Text("Age: 29")
+                        Text("Location: New York")
+                    }
                     .padding()
-            }
-            .padding()
-            
-            GroupBox(label: Label("User Information", systemImage: "person.circle")) {
-                VStack(alignment: .leading) {
-                    Text("Name: John Doe")
-                    Text("Age: 29")
-                    Text("Location: New York")
+                }
+                .groupBoxStyle(CustomGroupBoxStyle())
+                .padding()
+                
+                Grid {
+                    GridRow {
+                        Text("1")
+                        Text("2")
+                        Text("3")
+                    }
+                    GridRow {
+                        Text("4")
+                        Text("5")
+                        Text("6")
+                    }
+                    GridRow {
+                        Text("7")
+                        Text("8")
+                        Text("9")
+                    }
                 }
                 .padding()
             }
-            .groupBoxStyle(CustomGroupBoxStyle())
-            .padding()
-            
-            Grid {
-                GridRow {
-                    Text("1")
-                    Text("2")
-                    Text("3")
-                }
-                GridRow {
-                    Text("4")
-                    Text("5")
-                    Text("6")
-                }
-                GridRow {
-                    Text("7")
-                    Text("8")
-                    Text("9")
-                }
-            }
-            .padding()
         }
     }
 }
