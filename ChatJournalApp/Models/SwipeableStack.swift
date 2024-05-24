b8c7f79ebaf665c0fb9cd7711b4ba9738eeadee5
PianoMan <c0b211626f@edu.teu.ac.jp>
2024-05-24 14:49:22 +0900

    プレビューの画像の表示方法を横方向のスクロールに変更、テキストを押すと展開するように変更。

---
+++ ChatJournalApp/Models/SwipeableStack.swift
@@ -0,0 +1,8 @@
+//
+//  SwipeableStack.swift
+//  ChatJournalApp
+//
+//  Created by Takumi Yokawa on 2024/05/24.
+//
+
+import Foundation
--- ChatJournalApp/Views/JournalPreview.swift
+++ ChatJournalApp/Views/JournalPreview.swift
@@ -16,102 +16,43 @@ struct CustomGroupBoxStyle: GroupBoxStyle {
 }
 
 struct JournalPreview: View {
-    @State private var images: [String?] = ["circle.square","circle.square","circle.square","circle.square",nil]
+    @State private var isExpanded: Bool = false
+    @State private var images: [String?] = []
+    @State var diaryText: String
+
     var body: some View {
-        VStack {
-            GroupBox(label: Label("User Information", systemImage: "person.circle")) {
-                VStack{
-                    HStack{
-                        if let name = images[0] {
-                            Image(systemName: name)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(maxWidth: .infinity)
-                                .gridCellColumns(2)
-                        }
-                        VStack{
-                            HStack{
-                                if let name = images[1] {
-                                    Image(systemName: name)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(maxWidth: .infinity)
-                                        .gridCellColumns(2)
-                                }
-                                if let name = images[4] {
-                                    Image(systemName: name)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(maxWidth: .infinity)
-                                        .gridCellColumns(2)
-                                }
-                            }
-                            HStack{
-                                if let name = images[2] {
-                                    Image(systemName: name)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(maxWidth: .infinity)
-                                        .gridCellColumns(2)
-                                }
-                                if let name = images[3] {
-                                    Image(systemName: name)
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(maxWidth: .infinity)
-                                        .gridCellColumns(2)
-                                }
-                                
+        GroupBox(label: Label("User Information", systemImage: "person.circle")) {
+            VStack {
+                ScrollView(.horizontal) {
+                    HStack {
+                        ForEach(images, id: \.self) { image in
+                            if let image = image {
+                                Image(systemName: image)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(maxWidth: .infinity, maxHeight: 100)
                             }
                         }
-                        .padding()
-                        
                     }
-                    
                 }
-                .groupBoxStyle(DefaultGroupBoxStyle())
-                .padding()
                 
-                GroupBox {
-                    Text("This is another group without a label.")
-                        .padding()
-                }
-                .padding()
-                
-                GroupBox(label: Label("User Information", systemImage: "person.circle")) {
-                    VStack(alignment: .leading) {
-                        Text("Name: John Doe")
-                        Text("Age: 29")
-                        Text("Location: New York")
-                    }
+                Text(diaryText)
+                    .frame(maxWidth: .infinity)
+                    .lineLimit(isExpanded ? nil : 3)
                     .padding()
-                }
-                .groupBoxStyle(CustomGroupBoxStyle())
-                .padding()
-                
-                Grid {
-                    GridRow {
-                        Text("1")
-                        Text("2")
-                        Text("3")
+                    .onTapGesture {
+                        withAnimation {
+                            isExpanded.toggle()
+                        }
                     }
-                    GridRow {
-                        Text("4")
-                        Text("5")
-                        Text("6")
-                    }
-                    GridRow {
-                        Text("7")
-                        Text("8")
-                        Text("9")
-                    }
-                }
-                .padding()
             }
+            .frame(maxWidth: .infinity)
         }
+        .groupBoxStyle(DefaultGroupBoxStyle())
+        .padding()
     }
 }
 
 #Preview {
-        JournalPreview()
+    JournalPreview(diaryText: "こんにちは")
 }
